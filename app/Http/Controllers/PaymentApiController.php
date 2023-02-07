053eb2eb9485cb27ff1212eb719b983529dd0d96
Rendra Saputra <rendraseptian24@gmail.com>
2023-02-07 14:19:07 +0700

    update api payment

---
+++ app/Http/Controllers/PaymentApiController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PaymentApiController extends Controller
+{
+    public function index(Request $request)
+    {
+        if($rfid = $request->rfID)
+        {
+            $user = User::where('nomor_kartu',$rfid)->first();
+            if(empty($user))
+            {
+                return response()->json([
+                    'error' => 'Kartu tidak terdaftar'
+                ], 400);
+            }
+
+            if($user->saldo < 20000)
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Saldo anda tidak cukup'
+                ], 201);
+            }
+
+            $balance = $user->saldo - 20000;
+            $data = [
+                'id_user' => $user->id,
+                'total' => 20000,
+                'saldo' => $balance,
+            ];
+
+            $updateData = [
+                'saldo' => $balance,
+            ];
+
+
+            DB::transaction(function() use($data, $updateData, $user){
+
+            Order::create($data);
+
+            User::where('id', $user->id)->update($updateData);
+            
+            });
+            $dataUser = [
+                'nama' => $user->nama,
+                'saldo' => number_format($balance, 2),
+                'price' => 20000,
+            ];
+            
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Transaksi berhasil',
+                'data' => $dataUser
+            ], 201);
+        }
+
+        return response()->json([
+            'error' => 'Kartu tidak terdaftar'
+        ], 400);
+    }
+}
